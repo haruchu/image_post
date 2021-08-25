49fa823a0b66e9544cb902b959e633058b844c51
haru <haruki6180@gmail.com>
2021-08-25 16:25:40 +0900

    画像アップロード

--- image_post/post/models.py
+++ image_post/post/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(
+        validators=[FileExtensionValidator(['png', 'jpg'])], upload_to="images")
+    title = models.CharField(max_length=30)
+    file_name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.title)
--- image_post/post/views.py
+++ image_post/post/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.db.models.fields.files import ImageField
+from django.forms.forms import Form
+from django.shortcuts import render, redirect
+from .forms import ImageForm
+from .models import Image
+import uuid
 
 # Create your views here.
+
+def index(request):
+  form = ImageForm()
+  context = {
+    "form" : form,
+    "images" : Image.objects.order_by('-created_date').all(),
+  }
+  return render(request, 'index.html', context)
+
+
+def post(request):
+  form = ImageForm(request.POST, request.FILES)
+  if form.is_valid():
+    post = form.save(commit=False)
+    post.file_name = str(post.created_date)+ "_" + str(uuid.uuid4()) + ".png"
+    post = form.save()
+    return redirect('/')
+  return render(request, 'index.html', {'form': form})
